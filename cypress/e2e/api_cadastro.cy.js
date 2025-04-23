3afaabfd82cdf4a18dc8b8b604db4f7b1909abe9
Gerfson Alves <gerfson17@hotmail.com>
2025-04-23 16:54:59 -0300

    API cadastro

---
+++ cypress/e2e/api_cadastro.cy.js
@@ -0,0 +1,23 @@
+import { faker } from '@faker-js/faker';
+
+/// <reference types="cypress" />
+
+/// 01 Cadastrar usuários
+
+describe('API - Cadastro', () => {
+    it('Deve realizar o cadastro com sucesso', () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://serverest.dev/usuarios',
+            body: {
+                "nome": faker.person.fullName(),
+                "email": faker.internet.email(),
+                "password": "teste",
+                "administrador": "true"
+            }
+        }).then((response) => {
+            expect(response.status).to.equal(201)
+            expect(response.body.message).to.equal('Cadastro realizado com sucesso')
+        })
+    })
+})
