fcf9395cd671072d0f74b0c9d33ba2cc99b478e0
Gerfson Alves <gerfson17@hotmail.com>
2025-04-23 16:30:25 -0300

    API login

--- cypress/e2e/api_login.cy.js
+++ cypress/e2e/api_login.cy.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
+/// 01 Login com usuário válido - 02 Login com credenciais inválidas
+
+
 describe('API - Teste funcional de login', () => {
   it('Deve realizar o login com sucesso', () => {
     cy.request({
@@ -14,4 +17,19 @@ describe('API - Teste funcional de login', () => {
       expect(response.body.message).to.equal('Login realizado com sucesso')
     })
   })
+
+  it('Login com credenciais inválidas', () => {
+    cy.request({
+      method: 'POST',
+      url: 'https://serverest.dev/login',
+      body: {
+        "email": "fulano@qa.com",
+        "password": "teste1"
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.equal(401)
+      expect(response.body.message).to.equal('Email e/ou senha inválidos')
+    })
+  });
 })
